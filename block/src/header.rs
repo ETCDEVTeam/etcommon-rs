36b3b03a1388b214ff529fd4cbb3162c4d53fccf
Wei Tang <hi@that.world>
2018-03-14 20:10:43 +0800

    Remove blockchain-rs dependency

--- block/src/header.rs
+++ block/src/header.rs
@@ -2,9 +2,13 @@ use rlp::{self, Encodable, Decodable, RlpStream, DecoderError, UntrustedRlp};
 use bigint::{Address, Gas, H256, U256, B256, H64};
 use bloom::LogsBloom;
 use std::cmp::Ordering;
-use blockchain::chain::HeaderHash;
 use sha3::{Keccak256, Digest};
 use super::RlpHash;
+
+pub trait HeaderHash<H: Copy> {
+    fn parent_hash(&self) -> Option<H>;
+    fn header_hash(&self) -> H;
+}
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct TotalHeader(pub Header, U256);
